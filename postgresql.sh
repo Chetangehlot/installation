e3a659346a12acb264f10466f3289f29a4bb0379
satishsverma5 <satishsverma5@gmail.com>
2016-09-01 14:48:00 +0530

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,10 +1,11 @@
 # docker run -ti --name postgresql -v /data/postgresql:/var/lib/postgresql/9.4/main -p 5432:5432 ubuntu:14.04 bash
 
+apt-get update
+apt-get -y install vim wget
 #Postgresql install and run
 echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt-get update
-apt-get -y install vim wget
 apt-get install postgresql-9.4 postgresql-contrib-9.4
 apt-get install -y postgresql-9.4-postgis-2.1
 echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.4/main/pg_hba.conf
